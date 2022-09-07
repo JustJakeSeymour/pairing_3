b0c7d75b7c2c35586699a829aac474a1bf029281
J Seymour <JustJakeSeymour@gmail.com>
2022-09-06 18:55:41 -0600

    Initial commit

---
+++ friends.rb
@@ -0,0 +1,12 @@
+# define variable stores array
+# contains 8 integers btw 1-100
+prices = [25, 32, 64, 93, 7, 16, 75, 22]
+
+# program checks integer
+# each
+prices.each do |price|
+  # print >30
+  if price >= 30
+    then p price
+  end
+end
