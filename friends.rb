a37eea458a4ccc645d6e0acb3a5a994bdc2622d8
J Seymour <JustJakeSeymour@gmail.com>
2022-09-06 18:58:55 -0600

    edit to iteration practice

--- friends.rb
+++ friends.rb
@@ -6,7 +6,17 @@ prices = [25, 32, 64, 93, 7, 16, 75, 22]
 # each
 prices.each do |price|
   # print >30
-  if price >= 30
+  if price > 30
+    then p price
+  end
+end
+
+print "\n \n"
+# another method to check each
+
+prices.each do |price|
+  # print < 60
+  if price < 60
     then p price
   end
 end
